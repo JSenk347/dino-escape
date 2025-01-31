217d4f31bf7251f6626246fba33f036e4ab3c9d6
jmill753 <145062085+jmill753@users.noreply.github.com>
2025-01-31 11:47:20 -0700

    Create bitmaps.h

---
+++ bitmaps.h
@@ -0,0 +1,22 @@
+#ifndef BITMAPS_H
+#define BITMAPS_H
+#define HEIGHT_32 32
+
+typedef unsigned long UINT32;
+typedef unsigned int UINT16;
+
+extern const UINT32 dino_wdown_bitmap[HEIGHT_32];
+extern const UINT32 dino_wup_bitmap[HEIGHT_32];
+extern const UINT32 dino_dead_bitmap[HEIGHT_32];
+extern const UINT32 zero_bitmap[HEIGHT_32];
+extern const UINT32 one_bitmap[HEIGHT_32];
+extern const UINT32 two_bitmap[HEIGHT_32];
+extern const UINT32 three_bitmap[HEIGHT_32];
+extern const UINT32 four_bitmap[HEIGHT_32];
+extern const UINT32 five_bitmap[HEIGHT_32];
+extern const UINT32 six_bitmap[HEIGHT_32];
+extern const UINT32 seven_bitmap[HEIGHT_32];
+extern const UINT32 eight_bitmap[HEIGHT_32];
+extern const UINT32 nine_bitmap[HEIGHT_32];
+
+#endif // BITMAPS_H
