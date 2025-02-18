244815044f3baed14c324e25643bd54c54018970
Anna Gallup <agall087@mtroyal.ca>
2025-02-17 21:23:22 -0700

    updated score

--- TST_MODEL.C
+++ TST_MODEL.C
@@ -17,6 +17,6 @@ int main()
     Model init_all = {
         {(16,184),(16,184),(16,184),(16,184),0,0},   /* Dino variables */
         {},                                          /* Obs_wall variables */
-        {((311,359),0,TRUE),((343,359),0,FALSE),((375,359),0,FALSE),((407,359),0,FALSE)} /* Score variables */
+        {(311,359),(407,359),(311,391),(407,391),5000,0} /* Score variables */
     }; 
 }
