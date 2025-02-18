0adc723468d1100ff79daf28dc4b4a44d5641ccf
JSenk347 <jsenk347@mtroyal.ca>
2025-02-17 19:29:48 -0700

    updated objects

--- model.c
+++ model.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 
+#define HALF_GAP 25
 /*******************************************************************************
 	PURPOSE: To move the dino vertically up or down the screen, while staying
 		within the gameplay borders.
@@ -15,6 +16,7 @@
 			- 
 	OUTPUT: N/A
 *******************************************************************************/
+
 void move_dino(Dino *dino, int direction)
 {
 	dino->y += (dino->vert_velocity*dino->vert_direction);
@@ -33,4 +35,28 @@ void move_dino(Dino *dino, int direction)
 		dino->vert_velocity = 0;
 		dino->y = bottom_border - (dino_height + 1);
 	}
+}
+
+void init_obs_wall(Obs_wall *wall, unsigned int x, unsigned int gap_y) {
+	/* Bottom Obstacle */
+	wall -> bottom.top_left.x = x;
+	wall -> bottom.top_left.y = gap_y - HALF_GAP;
+	wall -> bottom.top_right.x = x + 31;
+	wall -> bottom.top_right.y = gap_y - HALF_GAP;
+
+	wall -> bottom.bot_left.x = 
+	wall -> bottom.bot_left.y = 
+	wall -> bottom.bot_right.x = 
+	wall -> bottom.bot_right.y =   
+	
+	/* Top Obstacle */
+	wall -> top.top_left.x =
+	wall -> top.top_left.y =
+	wall -> top.top_right.x =
+	wall -> top.top_right.y = 
+
+	wall -> top.bot_left.x = x;
+	wall -> top.bot_left.y = gap_y + HALF_GAP;
+	wall -> top.bot_right.x = x + 31;
+	wall -> top.bot_right.y = gap_y + HALF_GAP;
 }
