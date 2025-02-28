402efb37a9a71b98fb2a85ece9583fc741852ad4
JD-D3V <millsjd@telus.net>
2025-02-27 22:26:32 -0700

    files for stage 4

---
+++ render.c
@@ -0,0 +1,8 @@
+/*******************************************************************************
+ AUTHORS: Anna Running Rabbit, Jordan Senko, Joseph Mills
+ COURSE: COMP2659-001
+ INSTRUCTOR: Tim Reimer
+ DATE: Feb.24, 2025
+
+ SUMMARY: Contains functions for all rendering game graphics
+*******************************************************************************/
---
+++ render.h
@@ -0,0 +1,9 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#include "raster.h"
+#include "events.h"
+
+
+
+#endif
