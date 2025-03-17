30f0e4b7539c2530385f4fa47c4eb987bff7b109
JSenk347 <jsenk347@mtroyal.ca>
2025-03-17 12:16:58 -0600

    compiles now without get_time()

--- game_snd.c
+++ game_snd.c
@@ -1,17 +1,19 @@
 #include "game_snd.h"
 #include <osbind.h>
 
-void play_note() {
+void play_note(Note *note) {
 	volatile char *PSG_reg_select = 0xFF8800;
 	volatile char *PSG_reg_write  = 0xFF8802;
 	long old_ssp = Super(0);
 	
 	
 	*PSG_reg_select = 0;		/* set channel A fine tune = 248 */
-	*PSG_reg_write  = 248;      /* Higher pitch: decrease value */
+	/**PSG_reg_write  = 248;       Higher pitch: decrease value */
+	*PSG_reg_write = note -> fine;
 
-	*PSG_reg_select = 1;		/* set channel A coarse tune = 0 */
-	*PSG_reg_write  = 0;
+	*PSG_reg_select = 1;		/* set channel A coarse tune = 0 
+	*PSG_reg_write  = 0; */
+	*PSG_reg_write = note -> coarse;
 
 	*PSG_reg_select = 7;		/* enable channel A on mixer */
 	*PSG_reg_write  = 0x3E;
