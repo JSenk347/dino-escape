01d05090638f6bedbc1a6e1af2aa7468a7f8b165
Anna Gallup <agall087@mtroyal.ca>
2025-03-17 15:16:46 -0600

    added comments

--- dino_esc.c
+++ dino_esc.c
@@ -85,8 +85,8 @@ int main()
         time_elapsed = curr_time - prev_time;
         if (time_elapsed > 0) {
             /* PROCESS SYNCHRONOUS EVENTS */
-            process_input(&new_game, key);
-            key = NULL; /* Reset input key */
+            process_input(&new_game, key);  /* Moves dino */
+            key = NULL;                     /* Resets input key */
 
             move_walls(&new_game);
             check_collisions(&new_game);
